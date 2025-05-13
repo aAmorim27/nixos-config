990ba55586b01a85fa16a9db99fdc0dc91cf5b14
aAmorim27 <amorim.andre27@gmail.com>
2025-05-13 13:43:05 +0200

    zsh: manage plugins with omz

--- home/apps/zsh/default.nix
+++ home/apps/zsh/default.nix
@@ -1,30 +1,28 @@
 {pkgs, ...}:
 {
+  home.file.".zsh/plugins/you-should-use" = {
+    source = pkgs.fetchFromGitHub {
+      owner = "MichaelAquilina";
+      repo = "zsh-you-should-use";
+      rev = "1.9.0";
+      sha256 = "sha256-+3iAmWXSsc4OhFZqAMTwOL7AAHBp5ZtGGtvqCnEOYc0=";
+    };
+  };
+  
+  home.file.".zsh/themes/powerlevel10k" = {
+    source = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/";
+  };
+
   programs.zsh = {
     enable = true;
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
-    plugins = [
-      {
-        name = "zsh-powerlevel10k";
-        src = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/";
-        file = "powerlevel10k.zsh-theme";
-      }
-      {
-        name = "you-should-use";
-        file = "you-should-use.plugin.zsh";
-        src = pkgs.fetchFromGitHub {
-          owner = "MichaelAquilina";
-          repo = "zsh-you-should-use";
-          rev = "1.9.0";
-          sha256 = "sha256-+3iAmWXSsc4OhFZqAMTwOL7AAHBp5ZtGGtvqCnEOYc0=";
-        };
-      }
-    ];
     oh-my-zsh = {
       enable = true;
-      plugins = ["git" "nvm" "docker-compose" "docker"];
+      plugins = ["git" "nvm" "docker-compose" "docker" "you-should-use"];
+      custom= ".zsh";
+      theme= "powerlevel10k/powerlevel10k";
     };
     initExtra = ''
       source ~/.p10k.zsh
