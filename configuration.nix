611bf4ccda01208a2319428456f02976f7415e5c
aAmorim27 <amorim.andre27@gmail.com>
2025-05-14 23:24:52 +0200

    gnome: remove some unused apps

--- configuration.nix
+++ configuration.nix
@@ -27,7 +27,11 @@
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-
+  services.gnome = {
+    games.enable = false;
+  };
+  environment.gnome.excludePackages = with pkgs; [ gnome-tour ];
+  services.xserver.excludePackages = [ pkgs.xterm ];
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "fr";
