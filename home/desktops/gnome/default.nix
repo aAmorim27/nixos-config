aa510065b0b0113a993df6b41c1b3fd4f448949d
aAmorim27 <amorim.andre27@gmail.com>
2025-05-25 19:37:56 +0200

    gnome: started ricing

--- home/desktops/gnome/default.nix
+++ home/desktops/gnome/default.nix
@@ -1,14 +1,17 @@
-{pkgs, ...}:
+{pkgs, config, ...}:
 {
   home.packages = with pkgs; [
     gnomeExtensions.dock-from-dash
+    gnomeExtensions.open-bar
   ];
-
+  dconf.enable = true;
   dconf.settings = {
     "org/gnome/shell" = {
       disable-user-extensions = false;
       enabled-extensions = [
         "dock-from-dash@fthx"
+        "openbar@neuromorph"
+        "system-monitor@gnome-shell-extensions.gcampax.github.com"
       ];
     };
     "org/gnome/desktop/interface" = {
@@ -17,6 +20,18 @@
     };
     "org/gnome/desktop/sound" = {
       event-sounds= false;
+    };
+    "org/gnome/shell/extensions/openbar" = {
+      bartype = "Islands";
+      height = 45.0;
+      margin = 0.0;
+      neon = false;
+      bwidth = 0.0;
+      mbalpha = 0.0;
+      mshalpha = 0.0;
+    };
+    "org/gnome/shell/extensions/system-monitor" = {
+      show-swap = false;
     };
   };
 }
