df7791e0a9d0d5b34540ffd8f0a5ce2eaaa2fdfd
aAmorim27 <amorim.andre27@gmail.com>
2025-05-25 20:53:29 +0200

    gnome: continued ricing top bar

--- home/desktops/gnome/default.nix
+++ home/desktops/gnome/default.nix
@@ -1,4 +1,5 @@
-{pkgs, config, ...}:
+{pkgs, lib, ...}:
+with lib.hm.gvariant;
 {
   home.packages = with pkgs; [
     gnomeExtensions.dock-from-dash
@@ -29,6 +30,18 @@
       bwidth = 0.0;
       mbalpha = 0.0;
       mshalpha = 0.0;
+      iscolor = ["0.188" "0.204" "0.275"];
+      hcolor= ["0.255" "0.271" "0.349"];
+      fgcolor = ["0.776" "0.816" "0.961"];
+      mfgcolor = ["0.776" "0.816" "0.961"];
+      mbgcolor = ["0.161" "0.173" "0.235"];
+      mbgalpha = 0.85;
+      mhalpha = 0.20;
+      mscolor = ["0.906" "0.510" "0.518"];
+      autofg-bar = false;
+      autohg-bar = false;
+      autofg-menu = false;
+      autohg-menu = false;
     };
     "org/gnome/shell/extensions/system-monitor" = {
       show-swap = false;
