7f838d3db4270c4c21b427f38729f126bee53755
aAmorim27 <amorim.andre27@gmail.com>
2025-07-30 14:33:40 +0200

    nvf: enable more languages

--- home/apps/neovim/default.nix
+++ home/apps/neovim/default.nix
@@ -1,11 +1,22 @@
 {
   programs.nvf = {
     enable = true;
+    enableManpages = true;
     settings = {
       vim.viAlias = false;
       vim.vimAlias = true;
       vim.lsp = {
         enable = true;
+      };
+      vim.languages = 
+      {
+        ts.enable = true;
+        html.enable = true;
+        go.enable = true;
+        markdown.enable = true;
+        nix.enable = true;
+        python.enable = true;
+        rust.enable = true;
       };
     };
   };
